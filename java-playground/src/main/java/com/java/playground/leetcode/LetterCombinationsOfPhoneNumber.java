019988caf2e789928ba1f69d9cf70c3c0ec74301
Min Huang <min.huang@alumni.usc.edu>
2021-03-26 18:24:27 -0700

    Simpler solution

--- java-playground/src/main/java/com/java/playground/leetcode/LetterCombinationsOfPhoneNumber.java
+++ java-playground/src/main/java/com/java/playground/leetcode/LetterCombinationsOfPhoneNumber.java
@@ -1,6 +1,5 @@
 package com.java.playground.leetcode;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -15,44 +14,40 @@ import java.util.stream.Collectors;
  */
 public final class LetterCombinationsOfPhoneNumber {
   private final Map<Character, List<String>> map;
-  private final Map<String, List<String>> memo;
 
   public LetterCombinationsOfPhoneNumber() {
-    memo = new HashMap<>();
     map = new HashMap<>();
 
-    map.put('0', Arrays.asList(" "));
-    map.put('1', Arrays.asList());
-    map.put('2', Arrays.asList("a", "b", "c"));
-    map.put('3', Arrays.asList("d", "e", "f"));
-    map.put('4', Arrays.asList("g", "h", "i"));
-    map.put('5', Arrays.asList("j", "k", "l"));
-    map.put('6', Arrays.asList("m", "n", "o"));
-    map.put('7', Arrays.asList("p", "q", "r", "s"));
-    map.put('8', Arrays.asList("t", "u", "v"));
-    map.put('9', Arrays.asList("w", "x", "y", "z"));
+    map.put('0', List.of(" "));
+    map.put('1', List.of());
+    map.put('2', List.of("a", "b", "c"));
+    map.put('3', List.of("d", "e", "f"));
+    map.put('4', List.of("g", "h", "i"));
+    map.put('5', List.of("j", "k", "l"));
+    map.put('6', List.of("m", "n", "o"));
+    map.put('7', List.of("p", "q", "r", "s"));
+    map.put('8', List.of("t", "u", "v"));
+    map.put('9', List.of("w", "x", "y", "z"));
   }
 
-  // Remove the first digit.  Take the rest of the string, and generate all combinations of words using the remainder
-  // of the string.  Then, find the letters mapping to the first digit, and add them in front of all combinations.
   public List<String> letterCombinations(String digits) {
     if (digits.length() == 0) {
-      return Arrays.asList();
+      return List.of();
     }
 
     if (digits.length() == 1) {
       return map.get(digits.charAt(0));
     }
 
-    if (memo.containsKey(digits)) {
-      return memo.get(digits);
-    }
-
+    // First split the phone number into the first digit, and the rest of the digits.
     char head = digits.charAt(0);
     String rest = digits.substring(1);
-    List<String> combinations = letterCombinations(rest);
-    List<String> result = new LinkedList<>();
 
+    // Generate letter combinations for the result of the digits only, then add all possible letters from the first
+    // digit to the combinations generated with the remaining digits.
+    List<String> combinations = letterCombinations(rest);
+
+    List<String> result = new LinkedList<>();
     for (String letter : map.get(head)) {
       List<String> words = combinations.stream()
           .map(word -> letter + word)
@@ -60,7 +55,6 @@ public final class LetterCombinationsOfPhoneNumber {
       result.addAll(words);
     }
 
-    memo.put(digits, result);
     return result;
   }
 }
